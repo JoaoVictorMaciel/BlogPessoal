9ee8a1e92f9bb743a121349a5baf844e9079083e
Nilton Santos <jvictormaciel@yahoo.com.br>
2021-05-26 21:41:06 -0300

    Exercicio Blog Pessoal

---
+++ src/main/java/com/joao/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,13 @@
+package com.joao.blogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.joao.blogPessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
+}
