ec78ef52d78491220536537fb6d9032844853855
Nilton Santos <jvictormaciel@yahoo.com.br>
2021-05-28 19:57:31 -0300

    Versao 28-05

--- src/main/java/com/joao/blogPessoal/model/Postagem.java
+++ src/main/java/com/joao/blogPessoal/model/Postagem.java
@@ -6,8 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -24,7 +30,15 @@ public class Postagem {
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	
 	public long getId() {
 		return id;
 	}
@@ -48,6 +62,12 @@ public class Postagem {
 	}
 	public void setDate(Date date) {
 		this.date = date;
+	}
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
